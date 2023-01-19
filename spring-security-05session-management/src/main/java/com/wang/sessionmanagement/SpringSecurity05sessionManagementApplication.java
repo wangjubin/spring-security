af65753f7543c4008f3523f37af3c3e529180aee
‘wangjubin’ <wangjb@botech.com.cn>
2023-01-19 15:53:47 +0800

    security-session-management

---
+++ spring-security-05session-management/src/main/java/com/wang/sessionmanagement/SpringSecurity05sessionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wang.sessionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity05sessionManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity05sessionManagementApplication.class, args);
+    }
+
+}
---
+++ spring-security-05session-management/src/test/java/com/wang/sessionmanagement/SpringSecurity05sessionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.sessionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity05sessionManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
